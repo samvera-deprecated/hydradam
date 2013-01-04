fe5520b900bbf37b4105ce13cb18f15cad2f9e01
Justin Coyne <justin.coyne@yourmediashelf.com>
2013-01-04 12:19:44 -0600

    Validate that the directory exists

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,16 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  attr_accessible :directory
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :as => [:default, :admin]
+  attr_accessible :directory, as: :admin
+
+  validate :directory_must_exist
+
+  def directory_must_exist
+    unless directory.blank? || File.directory?(directory)
+      errors.add(:directory, "must be an existing directory")
+    end
+  end
 
   # Method added by Blacklight; Blacklight uses #to_s on your
   # user class to get a user-displayable login/identifier for
--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -4,6 +4,7 @@
 RailsAdmin.config do |config|
 
   config.authorize_with :cancan
+  config.attr_accessible_role { :admin }
 
 
   ################  Global configuration  ################
