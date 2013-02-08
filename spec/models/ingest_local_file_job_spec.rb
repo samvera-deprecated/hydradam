15c51d860653688f1864a9e46e67cf423faf40cc
Justin Coyne <justin.coyne@yourmediashelf.com>
2013-02-07 23:29:09 -0600

    Test for IngestLocalFileJob

---
+++ spec/models/ingest_local_file_job_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe IngestLocalFileJob do
+  before do
+    FileUtils.copy('spec/fixtures/world.png', 'tmp/')
+    @generic_file = GenericFile.new
+    @generic_file.apply_depositor_metadata('jcoyne@example.com')
+    @generic_file.save!
+    u = User.new
+    u.stub(:user_key).and_return('jcoyne@example.com')
+    User.stub(:find_by_user_key).with('jcoyne@example.com').and_return(u)
+  end
+  subject { IngestLocalFileJob.new @generic_file.id, 'tmp', 'world.png', 'jcoyne@example.com' }
+  it "should make an external datastream" do
+    subject.run
+    @generic_file.reload
+    @generic_file.content.controlGroup.should == 'E'
+    @generic_file.content.dsLocation.should match /file:\/\/\/.*\/spec\/storage\/..\/..\/..\/..\/.\/world\.png$/
+  end
+end
