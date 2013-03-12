b8e9acabf0d472a873b9e1cb3250aea10b6d2baf
Justin Coyne <justin@curationexperts.com>
2013-03-12 17:09:33 -0500

    Remove non-printing characters

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,9 +2,9 @@ class UserMailer < ActionMailer::Base
   default from: "no-reply@example.com"
   
   def file_online_notice(user, file)
-    @user = user
-    @url  = url_for(file)
-    mail(:to => user.email, :subject => "File ready for download")
+    @user = user
+    @url  = url_for(file)
+    mail(:to => user.email, :subject => "File ready for download")
   end
 
 end
