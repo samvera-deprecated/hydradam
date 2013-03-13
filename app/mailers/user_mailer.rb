3c6630592f17a03edf724f3d08325836edce83d5
Justin Coyne <justin@curationexperts.com>
2013-03-13 11:44:52 -0500

    Fixed online notice emails so they have links

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ActionMailer::Base
   def file_online_notice(user, file)
     @user = user
     @file = file
-    @url  = sufia.url_for(file)
+    @url = sufia.generic_file_url(file)
     mail(:to => user.email, :subject => "File ready for download")
   end
 
--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,5 +1,29 @@
 require "spec_helper"
 
 describe UserMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryGirl.create(:user) }
+  let(:file) do
+    gf = GenericFile.new
+    gf.apply_depositor_metadata('justin')
+    gf.save!
+    gf
+  end
+
+  let(:mail) { UserMailer.file_online_notice(user, file) }
+
+  it 'has the subject' do
+    mail.subject.should == 'File ready for download'
+  end
+
+  it 'has the receivers email' do
+    mail.to.should == [user.email]
+  end
+
+  it 'has the sender email' do
+    mail.from.should == ['no-reply@example.com']
+  end
+
+  it 'has a link to the file' do
+    mail.body.encoded.should match(/\/files\/#{file.noid}/)
+  end
 end
