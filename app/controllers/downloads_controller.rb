a4be6a026ede1d552e69be44a09dee4cf19d88c6
Justin Coyne <justin@curationexperts.com>
2013-03-25 21:57:53 -0700

    Use the range request in rubydora

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -22,12 +22,7 @@ class DownloadsController < ApplicationController
         end
       else
         # A proxy datastream
-        if request.headers["Range"].present?
-          logger.info "[DownloadsController] Range? #{request.headers["Range"]}"
-          stream(@asset)
-        else
-          send_content(@asset)
-        end
+        send_content(@asset)
       end
     else 
       logger.info "[DownloadsController] #{current_user ? current_user.user_key : 'anonymous user'} does not have access to read #{params['id']}"
@@ -43,7 +38,8 @@ class DownloadsController < ApplicationController
 
   protected
 
-  def stream(asset)
+  # Overriding so that we can use with external datastreams
+  def send_content(asset)
     ds = asset.datastreams[datastream_name]
     response.headers['Accept-Ranges'] = 'bytes'
 
@@ -55,60 +51,22 @@ class DownloadsController < ApplicationController
       return head :ok
     end
     
-    _, range = request.headers["Range"].split('bytes=')
-    from, to = range.split('-').map(&:to_i)
-    to = ds.dsSize - 1 unless to
-    logger.info "Range is #{from} - #{to}"
-    length = to - from + 1
-    response.headers['Content-Range'] = "bytes #{from}-#{to}/#{ds.dsSize}"
-    response.headers['Content-Length'] = "#{length}"
-    #send_data range(ds, from, length), content_options(asset, ds).merge(:status=>206)
-    self.status = 206
-    send_file_headers! content_options(asset, ds)
-    self.response_body = range(ds, from, length)#, content_options(asset, ds).merge(:status=>206)
-  end
-
-  def range (ds, from, length)
-    repo = ds.send(:repository) # TODO find a better way.
-    counter = 0
-    Enumerator.new do |blk|
-      repo.datastream_dissemination(pid: ds.pid, dsid: ds.dsid) do |response|
-        response.read_body do |chunk|
-          last_counter = counter
-          counter += chunk.size
-          if (counter > from) # greater than the range minimum
-            if counter > from + length
-              # At the end of what we need. Write the beginning of what was read.
-              offset = (length + from) - counter
-              blk << chunk[0..offset]
-              break
-            elsif from >= last_counter
-              # At the end of what we beginning of what we need. Write the end of what was read.
-              offset = from - last_counter
-              blk << chunk[offset..-1]
-            else 
-              # In the middle. We need all of this
-              blk << chunk
-            end
-            if (counter == from + length)
-              # Iteration was exactly the right length, no more reads needed.
-              break
-            end
-          end
-        end
-      end
-    end
-  end
-
-  # Overriding so that we can use with external datastreams
-  def send_content(asset)
-    ds = asset.datastreams[datastream_name]
-    raise ActionController::RoutingError.new('Not Found') if ds.nil? or !ds.has_content?
-    # ds.filename is only for externals
-    if (ds.respond_to? :filename)
+    if request.headers["Range"]
+      _, range = request.headers["Range"].split('bytes=')
+      from, to = range.split('-').map(&:to_i)
+      to = ds.dsSize - 1 unless to
+      logger.info "Range is #{from} - #{to}"
+      length = to - from + 1
+      response.headers['Content-Range'] = "bytes #{from}-#{to}/#{ds.dsSize}"
+      response.headers['Content-Length'] = "#{length}"
+      self.status = 206
+      send_file_headers! content_options(asset, ds)
+      self.response_body = ds.stream(from, length)
+    elsif (ds.respond_to? :filename)
       send_file ds.filename, content_options(asset, ds)
     else
-      send_data ds.content, content_options(asset, ds)
+      send_file_headers! content_options(asset, ds)
+      self.response_body = ds.stream
     end
   end
 
