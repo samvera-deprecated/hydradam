8986cf150fa3c7d836beecad02cb92b7db9cd161
Justin Coyne <justin@curationexperts.com>
2013-07-09 12:33:14 -0300

    Make a broken test pending

--- spec/controllers/batch_edits_controller_spec.rb
+++ spec/controllers/batch_edits_controller_spec.rb
@@ -43,7 +43,7 @@ describe BatchEditsController do
       controller.should_receive(:can?).with(:edit, @two.pid).and_return(true)
     end
     it "should be successful" do
-      pending "MZ copied this test from Sufia. It doesn't work their either. See https://github.com/projecthydra/sufia/commit/7a626dea2e50f60782a510caa4a3b37adf8f0296#commitcomment-3579990"
+      pending "MZ copied this test from Sufia. It doesn't work there either. See https://github.com/projecthydra/sufia/commit/7a626dea2e50f60782a510caa4a3b37adf8f0296#commitcomment-3579990"
       put :update , update_type:"delete_all"
       response.should be_redirect
       expect {GenericFile.find(@one.id)}.to raise_error ActiveFedora::ObjectNotFoundError
