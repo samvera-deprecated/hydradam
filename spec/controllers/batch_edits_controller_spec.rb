4ccd7aed9a47da5a6c6d92cd298eaa6d6e139531
Justin Coyne <justin@curationexperts.com>
2013-07-15 14:33:07 -0500

    Apparently in rspec 2.14 expectations are not canceled by a pending test

--- spec/controllers/batch_edits_controller_spec.rb
+++ spec/controllers/batch_edits_controller_spec.rb
@@ -39,11 +39,11 @@ describe BatchEditsController do
       @one.save!
       @two.save!
       controller.batch = [@one.pid, @two.pid]
-      controller.should_receive(:can?).with(:edit, @one.pid).and_return(true)
-      controller.should_receive(:can?).with(:edit, @two.pid).and_return(true)
     end
     it "should be successful" do
       pending "MZ copied this test from Sufia. It doesn't work there either. See https://github.com/projecthydra/sufia/commit/7a626dea2e50f60782a510caa4a3b37adf8f0296#commitcomment-3579990"
+      controller.should_receive(:can?).with(:edit, @one.pid).and_return(true)
+      controller.should_receive(:can?).with(:edit, @two.pid).and_return(true)
       put :update , update_type:"delete_all"
       response.should be_redirect
       expect {GenericFile.find(@one.id)}.to raise_error ActiveFedora::ObjectNotFoundError
