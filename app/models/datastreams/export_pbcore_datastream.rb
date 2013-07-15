1e95da99496ead42e4ce11891cdd99b5e2198044
Justin Coyne <justin@curationexperts.com>
2013-07-15 14:11:54 -0500

    Om 3.0.2 can inherit registries and terminologies

--- app/models/datastreams/export_pbcore_datastream.rb
+++ app/models/datastreams/export_pbcore_datastream.rb
@@ -1,17 +1,4 @@
 class ExportPbcoreDatastream < HydraPbcore::Datastream::Document
-  def self.template_registry
-    # This should be inheritable from the superclass.
-    # This is a temporary (hopefully) hack to make it work
-    HydraPbcore::Datastream::Document.template_registry
-  end
-
-  def self.terminology
-    # This should be inheritable from the superclass.
-    # This is a temporary (hopefully) hack to make it work
-    HydraPbcore::Datastream::Document.terminology
-  end
-
-
   def title(type, value)
     ng_xml_will_change!
     ng_xml.root.add_child("<pbcoreTitle titleType=\"#{Array(type).first}\">#{Array(value).first}</pbcoreTitle>")
