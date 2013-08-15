a5ab0dba9cf2688729bade0f9cc967c48733a5bc
Justin Coyne <justin@curationexperts.com>
2013-08-15 12:39:38 -0500

    Removed bad spec

--- spec/controllers/batch_edits_controller_spec.rb
+++ spec/controllers/batch_edits_controller_spec.rb
@@ -31,26 +31,4 @@ describe BatchEditsController do
           :originating_department, :subject, :language, :rights, :tag, :related_url]
     end
   end
-  
-  describe "update" do
-    before do
-      @one = GenericFile.new(creator_attributes:[{name: "Fred"}], :language=>'en')
-      @one.apply_depositor_metadata('mjg36')
-      @two = GenericFile.new(creator_attributes:[{name: "Wilma"}], publisher_attributes:[{name:'Rand McNally'}], :language=>'en')
-      @two.apply_depositor_metadata('mjg36')
-      @one.save!
-      @two.save!
-      controller.batch = [@one.pid, @two.pid]
-    end
-    it "should be successful" do
-      pending "MZ copied this test from Sufia. It doesn't work there either. See https://github.com/projecthydra/sufia/commit/7a626dea2e50f60782a510caa4a3b37adf8f0296#commitcomment-3579990"
-      controller.should_receive(:can?).with(:edit, @one.pid).and_return(true)
-      controller.should_receive(:can?).with(:edit, @two.pid).and_return(true)
-      put :update , update_type:"delete_all"
-      response.should be_redirect
-      expect {GenericFile.find(@one.id)}.to raise_error ActiveFedora::ObjectNotFoundError
-      expect {GenericFile.find(@two.id)}.to raise_error ActiveFedora::ObjectNotFoundError
-    end
-  end
-
 end
