4ec6fb4e162d063e7e857f6abba68efef1644503
Andrew Myers <afredmyers@gmail.com>
2015-04-30 09:55:28 -0400

    Wraps spec for ExportPbcoreDatastream#title in a `describe` block.

--- spec/models/datastreams/export_pbcore_datastream_spec.rb
+++ spec/models/datastreams/export_pbcore_datastream_spec.rb
@@ -1,16 +1,20 @@
 require 'spec_helper'
 
 describe ExportPbcoreDatastream do
-  it "should have title with a type argument" do
-    subject.title('Program', "Sample Program")
-    subject.title('Series', "Sample Series")
-    subject.title('Item', "Sample Item")
-    subject.title('Episode', "Sample Episode")
 
-    xml = subject.ng_xml
-    expect(xml.xpath('/pbcoreDescriptionDocument/pbcoreTitle[@titleType="Program"]').text).to eq "Sample Program"
-    expect(xml.xpath('/pbcoreDescriptionDocument/pbcoreTitle[@titleType="Series"]').text).to eq "Sample Series"
-    expect(xml.xpath('/pbcoreDescriptionDocument/pbcoreTitle[@titleType="Item"]').text).to eq "Sample Item"
-    expect(xml.xpath('/pbcoreDescriptionDocument/pbcoreTitle[@titleType="Episode"]').text).to eq "Sample Episode"
+  describe '#title' do
+    it "should have title with a type argument" do
+
+      subject.title('Program', "Sample Program")
+      subject.title('Series', "Sample Series")
+      subject.title('Item', "Sample Item")
+      subject.title('Episode', "Sample Episode")
+
+      xml = subject.ng_xml
+      expect(xml.xpath('/pbcoreDescriptionDocument/pbcoreTitle[@titleType="Program"]').text).to eq "Sample Program"
+      expect(xml.xpath('/pbcoreDescriptionDocument/pbcoreTitle[@titleType="Series"]').text).to eq "Sample Series"
+      expect(xml.xpath('/pbcoreDescriptionDocument/pbcoreTitle[@titleType="Item"]').text).to eq "Sample Item"
+      expect(xml.xpath('/pbcoreDescriptionDocument/pbcoreTitle[@titleType="Episode"]').text).to eq "Sample Episode"
+    end
   end
 end
